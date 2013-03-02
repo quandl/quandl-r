0d5a5f24ff26347b3f58f5abd3a0c5fe145417a7
Gergely Daróczi <gergely@snowl.net>
2013-03-02 13:24:54 +0100

    simplified `if` conditions

--- R/Quandl.R
+++ R/Quandl.R
@@ -3,6 +3,7 @@ auth_token <- NULL
 Quandl <- function(code, type="raw",start_date=NULL,end_date=NULL,transformation="",collapse="",authcode=auth_token)
 {
 
+    ## Check if data is available & grab metadata (although it's one extra API request)
     string = paste("http://www.quandl.com/api/v1/datasets/", code, ".xml?", "&rows=0", sep="")
     if(is.null(authcode))
         warning("It would appear you aren't using an authentication token. Please visit http://www.quandl.com/help/r or your usage may be limited.")
@@ -19,35 +20,25 @@ Quandl <- function(code, type="raw",start_date=NULL,end_date=NULL,transformation
     freq      = pmatch(frequency,c("annual","quarterly","monthly"),nomatch=10)
     freq      = as.integer(2.5*freq^2-4.5*freq+3)
 
-    ## Add auth_token if available
-    if(is.null(authcode))
-    {
-        string = paste("http://www.quandl.com/api/v1/datasets/",code,".csv?&sort_order=asc",sep="")
-    }
-    else
-    {
-        string = paste("http://www.quandl.com/api/v1/datasets/",code,".csv?&sort_order=asc&auth_token=",authcode,sep="")
-    }
+    ## Build API URL and add auth_token if available
+    string = paste("http://www.quandl.com/api/v1/datasets/", code, ".csv?&sort_order=asc", sep="")
+    if (!is.null(authcode))
+        paste(string, "&auth_token=", authcode, sep = "")
 
-    ## API options
-    if(!is.null(start_date))
-    {
-        string = paste(string,"&trim_start=",as.Date(start_date),sep="")
-    }
-    if(!is.null(end_date))
-    {
+    ## Add API options
+    if (!is.null(start_date))
+        string = paste(string, "&trim_start=", as.Date(start_date), sep="")
+    if (!is.null(end_date))
         string = paste(string,"&trim_end=",as.Date(end_date),sep="")
-    }
-    if(!is.na(pmatch(transformation,c("diff","rdiff","normalize","cumul"))))
-    {
+    if (!is.na(pmatch(transformation, c("diff", "rdiff", "normalize", "cumul"))))
         string = paste(string,"&transformation=",transformation,sep="")
-    }
-    if(!is.na(pmatch(collapse,c("weekly","monthly","quarterly","annual"))))
-    {
+    if (!is.na(pmatch(collapse, c("weekly", "monthly", "quarterly", "annual")))) {
         string = paste(string,"&collapse=",collapse,sep="")
         freq = pmatch(collapse,c("annual","quarterly","monthly"),nomatch=10)
         freq = as.integer(2.5*freq^2-4.5*freq+3)
     }
+
+    ## Fetch data
     data = read.csv(string)
     data[,1] = as.Date(data[,1])
 

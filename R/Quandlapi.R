d7fcd4b85329b1d0c64b15bf56aca066a543e4b2
John Malc <cincenko@outlook.com>
2015-03-06 15:50:35 +0100

    Revert "Develop"

--- R/Quandlapi.R
+++ R/Quandlapi.R
@@ -15,10 +15,23 @@
 #' quandldata = quandl.api(version="v1", path="datasets/NSE/OIL", http="GET")
 #' plot(quandldata[,1])
 #' }
+#' @importFrom RCurl getURL
+#' @importFrom RCurl postForm
+#' @importFrom RCurl httpDELETE
+#' @importFrom RCurl basicHeaderGatherer
+#' @importFrom RJSONIO fromJSON
 #' @export
-quandl.api <- function(version="v1", path, http = c('GET', 'PUT', 'POST', 'DELETE'), postdata = NULL, ...) {
-
+quandl.api <- function(version="v1", path, http = c('GET', 'PUT', 'POST', 'DELETE'), ...) {
+  headers <- basicHeaderGatherer()
   params <- list(...)
+
+  if(http == 'PUT' || http == 'POST') {
+    postdata <- params$postdata
+    if (length(which(names(params) == "postdata")) == 0) {
+      stop("No post data entered")
+    }
+    params[[which(names(params) == "postdata")]] <- NULL
+  }
 
   params$request_source <- 'R'
   params$request_version <- Quandl.version
@@ -26,38 +39,58 @@ quandl.api <- function(version="v1", path, http = c('GET', 'PUT', 'POST', 'DELET
   http <- match.arg(http)
   request_url <- paste(paste(Quandl.host, version, path, sep="/"), "?", sep="")
   param_names <- names(params)
+
   if(length(params) > 0) {
     for(i in 1:length(params)) {
       request_url <- paste(request_url, "&", param_names[i], "=", params[[i]], sep="")
     }
   }
 
-
+  #print(request_url)
   switch(http,
          GET={
-           response <- httr::GET(request_url, nullValue=as.numeric(NA))
+           response <- getURL(request_url, customRequest = "GET", headerfunction=headers$update, curl = Quandl.curlopts())
          },
          PUT={
-           response <- httr::PUT(request_url, body=postdata)
+           response <- getURL(request_url, customRequest = "PUT", headerfunction=headers$update,
+                              httpheader=c("Content-Length"=nchar(postdata, type="bytes"), "Content-Type"="application/json"), postfields=postdata, curl = Quandl.curlopts())
          },
          POST={
-           response <- httr::POST(request_url, body=postdata)
+           response <- postForm(request_url, .params=postdata, curl = Quandl.curlopts())
          },
          DELETE={
-           response <- httr::DELETE(request_url)
+           response <- httpDELETE(request_url, headerfunction=headers$update, curl = Quandl.curlopts())
          }
   )
+  is.error = FALSE
 
+  if(http %in% c('GET', 'PUT', 'DELETE')) {
+    status <- try(headers$value()[["status"]], silent = TRUE)
+    if (inherits(status, 'try-error')) {
+      stop("I am sorry but Quandl is down for maintenance. Please check the main website for status updates.", call. = FALSE)
+    }
 
-  if(httr::status_code(response) == 500) {
-    stop("Sorry but Quandl is currently down. Please visit our twitter (@quandl) for more information.", call. = FALSE)
-  } else if (httr::status_code(response) != 200) {
-    stop(httr::content(response), call. = FALSE)
-  } else {
-    return(httr::content(response, simplifyVector = TRUE))
+    if (length(grep("200", status))) {
+    } else {
+      is.error = TRUE
+    }
   }
 
+  json = try(fromJSON(response, nullValue = as.numeric(NA)), silent = TRUE)
+  if (inherits(json, 'try-error')) {
+    if(is.error) {
+      stop(response, call. = FALSE)
+    } else {
+      print(response)
+      print(request_url)
+      stop("Malformed JSON")
+    }
+  }
+
+  if (is.error) {
+    stop(json, call. = FALSE)
+  } else {
+    return(json)
+  }
 
 }
-
-
